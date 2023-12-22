8c0a737af549b0a2c614f9cd9a670a39e115b21c
MaxiSound <maxisound@yandex.ru>
2023-12-22 16:18:44 +0700

    ready

---
+++ site/main/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Offer(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Предложение'
+        verbose_name_plural = 'Предложения'
